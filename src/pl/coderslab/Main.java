6f5070570435251ec135da7a8350e7bf90c33f71
genno@gazeta.pl <genno@gazeta.pl>
2019-03-09 12:32:34 +0100

    klasa Exercise

--- src/pl/coderslab/Main.java
+++ src/pl/coderslab/Main.java
@@ -1,7 +1,59 @@
 package pl.coderslab;
 
+import pl.coderslab.model.User;
+import pl.coderslab.util.DbUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+       // System.out.println("test");
+
+        //User user = new User("grzegorz.szczerba", "test2@gazeta.pl", "123456789");
+        try (Connection connection = DbUtil.getConnection()) {
+            //user.saveToDB(connection);
+
+          //  user = User.loadUserById(connection, 6);
+           // user.delete(connection);
+            /*user.setUsername("Pankracy2");
+            user.setEmail("dssdfd@fsdfd.pl");
+            user.saveToDB(connection);
+
+            User[] users = User.loadAllUsers(connection);
+            for (User userElement : users) {
+                System.out.println(userElement);
+            }*/
+
+            Exercise exercise = new Exercise("testowy exercise", "jakis tam description");
+            //exercise.saveToDB(connection);
+            //Exercise.loadExerciseById(connection, 1);
+            //exercise.toString();
+            //System.out.println(exercise);
+            //exercise.saveToDB(connection);
+            //exercise.setTitle("nowy testowy title");
+            exercise = Exercise.loadExerciseById(connection, 5);
+            exercise.delete(connection);
+
+
+
+            Exercise[] exercises = Exercise.loadAllExercise(connection);
+            for (Exercise exerciseElement : exercises) {
+                System.out.println(exerciseElement);
+            }
+
+
+
+
+            /*User user = new User("test", "tedsfd@fdgg.pl", "dsfdgg");
+            User.loadUserById(connection, 1);
+            user.toString();*/
+
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
---
+++ src/pl/coderslab/util/DbUtil.java
@@ -0,0 +1,12 @@
+package pl.coderslab.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbUtil {
+    public static Connection getConnection()  throws SQLException {
+         return DriverManager.getConnection("jdbc:mysql://localhost:3306/warsztatdrugi?useSSL=false&characterEncoding=utf8", "root", "coderslab");
+
+        }
+}
